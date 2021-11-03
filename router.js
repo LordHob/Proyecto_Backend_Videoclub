2d5d779e23ca73436b36094a290edb70eb37368b
LordHob <rafaelginergomez@gmail.com>
2021-11-03 08:55:52 +0100

    cambios menores

--- router.js
+++ router.js
@@ -9,11 +9,11 @@ const MovieRouter = require('./routes/movie.routes');
 const UserRouter = require('./routes/user.router');
 
 //Rutas
-router.use('/api', auth, UserRouter); //Login and register routes
-router.use('/order', auth, OrderRouter); // CRUD orders
-router.use('/movie', auth, MovieRouter); // CRUD movies
+router.use('/api', UserRouter); //Login and register routes
+router.use('/order', OrderRouter); // CRUD orders
+router.use('/movie', MovieRouter); // CRUD movies
 
 
-//Faltaba por poner el "auth" delante de los archivos Router, para impedir que se pueda acceder sin autentificación. Para hacer que funcione, quitar los "auth", crear un usuario, copiar el token, usarlo en Postman (Authorization - Type: Barear Token), y luego ya volver a ponerlos (los "auth").
+//Faltaba por poner el "auth" delante de los archivos Router, para impedir que se pueda acceder sin autentificación. Para hacer que funcione, quitar los "auth", crear un usuario, copiar el token, usarlo en Postman (Authorization - Type: Barear Token), y luego ya volver a ponerlos (los "auth"). (, auth)
 
 module.exports = router;
