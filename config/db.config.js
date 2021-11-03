5287eedbd1e7062decfff007f1acd835edac500a
LordHob <rafaelginergomez@gmail.com>
2021-11-03 11:19:09 +0100

    Actualizado archivo gitignore

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
     url:  //Cadena de conexión mongo db
-    "mongodb://root:game402510@192.168.1.77:27017/Backendvideoclub?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"  };
+    "mongodb://root:game402510@172.16.10.172:27017/Backendvideoclub?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"  };
